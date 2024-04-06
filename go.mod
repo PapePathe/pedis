e3bb76ebc6b50c7f8538d18430eacce878d7b029
Papa Pathé SENE <pathe.sene@gmail.com>
2024-04-06 07:18:17 +0000

    feat: add raft from etcd key value store example

--- go.mod
+++ go.mod
@@ -9,13 +9,18 @@ require (
 
 require (
 	github.com/armon/go-metrics v0.4.1 // indirect
+	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/coreos/go-semver v0.3.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/go-playground/assert/v2 v2.2.0 // indirect
 	github.com/go-redis/redis v6.15.9+incompatible // indirect
-	github.com/google/btree v0.0.0-20180813153112-4030bb1f1f0c // indirect
+	github.com/gogo/protobuf v1.3.2 // indirect
+	github.com/golang/protobuf v1.5.4 // indirect
+	github.com/google/btree v1.0.1 // indirect
 	github.com/hashicorp/errwrap v1.0.0 // indirect
 	github.com/hashicorp/go-hclog v1.6.2 // indirect
 	github.com/hashicorp/go-immutable-radix v1.0.0 // indirect
@@ -29,13 +34,32 @@ require (
 	github.com/hashicorp/serf v0.10.1 // indirect
 	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/miekg/dns v1.1.41 // indirect
 	github.com/onsi/ginkgo v1.16.5 // indirect
 	github.com/onsi/gomega v1.32.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/prometheus/client_golang v1.11.1 // indirect
+	github.com/prometheus/client_model v0.2.0 // indirect
+	github.com/prometheus/common v0.26.0 // indirect
+	github.com/prometheus/procfs v0.6.0 // indirect
 	github.com/redis/go-redis/v9 v9.5.1 // indirect
 	github.com/sean-/seed v0.0.0-20170313163322-e2103e2c3529 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
+	github.com/xiang90/probing v0.0.0-20190116061207-43a291ad63a2 // indirect
+	go.etcd.io/etcd/api/v3 v3.6.0-alpha.0 // indirect
+	go.etcd.io/etcd/client/pkg/v3 v3.6.0-alpha.0 // indirect
+	go.etcd.io/etcd/pkg/v3 v3.6.0-alpha.0 // indirect
+	go.etcd.io/etcd/raft/v3 v3.6.0-alpha.0 // indirect
+	go.etcd.io/etcd/server/v3 v3.6.0-alpha.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.7.0 // indirect
+	go.uber.org/zap v1.17.0 // indirect
 	golang.org/x/sys v0.18.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.0.0-20210220033141-f8bda1e9f3ba // indirect
+	google.golang.org/genproto v0.0.0-20211118181313-81c1377c94b1 // indirect
+	google.golang.org/grpc v1.41.0 // indirect
+	google.golang.org/protobuf v1.33.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
---
+++ praft/kvstore.go
@@ -0,0 +1,217 @@
+// Copyright 2015 The etcd Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package praft
+
+import (
+	"bytes"
+	"encoding/gob"
+	"encoding/json"
+	"log"
+	"net"
+	"pedis/internal/commands"
+	"pedis/internal/storage"
+	"strings"
+	"sync"
+
+	"go.etcd.io/etcd/raft/v3/raftpb"
+	"go.etcd.io/etcd/server/v3/etcdserver/api/snap"
+)
+
+type RedisCommand interface {
+	Run([]byte, net.Conn, storage.Storage)
+}
+
+// a key-value store backed by raft
+type PedisServer struct {
+	proposeC    chan<- string // channel for proposing updates
+	mu          sync.RWMutex
+	kvStore     map[string]string // current committed key-value pairs
+	snapshotter *snap.Snapshotter
+
+	handlers map[string]RedisCommand
+	store    storage.Storage
+	addr     string
+
+	storageProposeChan chan storage.StorageData
+}
+
+func NewKVStore(
+	snapshotter *snap.Snapshotter,
+	proposeC chan<- string,
+	commitC <-chan *commit,
+	errorC <-chan error,
+	store storage.Storage,
+	pedisAddr string,
+	storageProposeChan chan storage.StorageData,
+) *PedisServer {
+	s := &PedisServer{
+		proposeC:           proposeC,
+		kvStore:            make(map[string]string),
+		snapshotter:        snapshotter,
+		handlers:           make(map[string]RedisCommand),
+		addr:               pedisAddr,
+		store:              store,
+		storageProposeChan: storageProposeChan,
+	}
+	snapshot, err := s.loadSnapshot()
+	if err != nil {
+		log.Panic(err)
+	}
+	if snapshot != nil {
+		log.Printf("loading snapshot at term %d and index %d", snapshot.Metadata.Term, snapshot.Metadata.Index)
+		if err := s.recoverFromSnapshot(snapshot.Data); err != nil {
+			log.Panic(err)
+		}
+	}
+
+	_ = s.AddHandler("*", commands.RequestHandler{})
+	// read commits from raft into kvStore map until error
+	go s.readCommits(commitC, errorC)
+	go s.readProposeChan()
+	return s
+}
+
+func (rs *PedisServer) AddHandler(firstByte string, c RedisCommand) error {
+	rs.handlers[firstByte] = c
+
+	return nil
+}
+func (s *PedisServer) StartPedis() error {
+	listener, err := net.Listen("tcp", s.addr)
+	if err != nil {
+		return err
+	}
+
+	defer listener.Close()
+
+	for {
+		log.Println("received new connection")
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Println("Error accepting connection:", err)
+		}
+
+		go s.handleConnection(conn)
+	}
+}
+
+func (rs *PedisServer) handleConnection(conn net.Conn) {
+	for {
+		b := make([]byte, 1024)
+
+		size, err := conn.Read(b)
+
+		if err != nil || size == 0 {
+			conn.Close()
+			break
+		}
+
+		commandId := string(b[0])
+		handler, commandNotFound := rs.handlers[commandId]
+
+		if !commandNotFound {
+			log.Println(err)
+			continue
+		}
+
+		handler.Run(b[1:size], conn, rs.store)
+	}
+}
+
+func (s *PedisServer) Lookup(key string) (string, bool) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	v, ok := s.kvStore[key]
+	return v, ok
+}
+
+func (s *PedisServer) Propose(data storage.StorageData) {
+	var buf strings.Builder
+	if err := gob.NewEncoder(&buf).Encode(data); err != nil {
+		log.Fatal(err)
+	}
+
+	s.proposeC <- buf.String()
+}
+
+func (s *PedisServer) readProposeChan() {
+	for propose := range s.storageProposeChan {
+		s.Propose(propose)
+	}
+}
+func (s *PedisServer) readCommits(commitC <-chan *commit, errorC <-chan error) {
+	log.Println("reading commits")
+	for commit := range commitC {
+		if commit == nil {
+			// signaled to load snapshot
+			snapshot, err := s.loadSnapshot()
+			if err != nil {
+				log.Panic(err)
+			}
+			if snapshot != nil {
+				log.Printf("loading snapshot at term %d and index %d", snapshot.Metadata.Term, snapshot.Metadata.Index)
+				if err := s.recoverFromSnapshot(snapshot.Data); err != nil {
+					log.Panic(err)
+				}
+			}
+			continue
+		}
+
+		for _, data := range commit.data {
+			var dataKv storage.StorageData
+			dec := gob.NewDecoder(bytes.NewBufferString(data))
+			if err := dec.Decode(&dataKv); err != nil {
+				log.Fatalf("raftexample: could not decode message (%v)", err)
+			}
+			err := s.store.WriteFromRaft(dataKv)
+			if err != nil {
+				log.Println("error writing proposed change from raft", err)
+			}
+			log.Println("length of store", len(s.kvStore), data)
+		}
+		close(commit.applyDoneC)
+	}
+	if err, ok := <-errorC; ok {
+		log.Fatal(err)
+	}
+}
+
+func (s *PedisServer) GetSnapshot() ([]byte, error) {
+	s.mu.RLock()
+	defer s.mu.RUnlock()
+	return json.Marshal(s.kvStore)
+}
+
+func (s *PedisServer) loadSnapshot() (*raftpb.Snapshot, error) {
+	snapshot, err := s.snapshotter.Load()
+	if err == snap.ErrNoSnapshot {
+		return nil, nil
+	}
+	if err != nil {
+		return nil, err
+	}
+	return snapshot, nil
+}
+
+func (s *PedisServer) recoverFromSnapshot(snapshot []byte) error {
+	var store map[string]string
+	if err := json.Unmarshal(snapshot, &store); err != nil {
+		return err
+	}
+	s.mu.Lock()
+	defer s.mu.Unlock()
+	s.kvStore = store
+	return nil
+}
