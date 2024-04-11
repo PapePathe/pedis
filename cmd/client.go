3f1029976d0c2a889c4c14fd204cf14075a64387
Papa Pathé SENE <pathe.sene@gmail.com>
2024-04-11 15:38:09 +0000

    allow to specify number of requests

--- cmd/client.go
+++ cmd/client.go
@@ -2,20 +2,26 @@ package main
 
 import (
 	"context"
+	"flag"
 	"fmt"
 	"log"
 
 	"github.com/redis/go-redis/v9"
 )
 
+var reqCount int
+
 func main() {
+	flag.IntVar(&reqCount, "req", 100, "")
+	flag.Parse()
+
 	client := redis.NewClient(&redis.Options{
 		Addr:     "localhost:6379",
 		Password: "",
 		DB:       0,
 	})
 
-	for i := 0; i < 2; i++ {
+	for i := 0; i < reqCount; i++ {
 		key := fmt.Sprintf("key-%d", i)
 		err := client.Set(context.Background(), key, "value", 0).Err()
 		if err != nil {
@@ -29,5 +35,19 @@ func main() {
 		}
 
 		log.Println("Get result", result)
+
+		key = fmt.Sprintf("hset:key-%d", i)
+		err = client.HSet(context.Background(), key, "name", "pathe").Err()
+		if err != nil {
+			log.Println(err)
+		}
+
+		resultHset, err := client.HGet(context.Background(), key, "name").Result()
+
+		if err != nil {
+			log.Println(err)
+		}
+
+		log.Println("Hset result", resultHset)
 	}
 }
